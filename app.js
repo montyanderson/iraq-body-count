eeaa98e869491de133c21074921e0ecc00281055
Monty Anderson <monty.linux@gmail.com>
2016-10-06 22:04:44 +0100

    allows you to search

--- app.js
+++ app.js
@@ -2,6 +2,32 @@ const express = require("express");
 const search = require("./search");
 const app = express();
 
+app.get("/", (req, res) => {
+	res.end(`
+<!DOCTYPE html>
+<html>
+	<head>
+
+	</head>
+	<body>
+
+		<input id="search">
+		<div class="results"></div>
+
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
+		<script>
+		$("#search").keyup(function() {
+			$.getJSON("/search?q=" + $(this).val(), function(res) {
+				$(".results").html(JSON.stringify(res));
+			});
+		});
+		</script>
+
+	</body>
+</html>
+`).trim()
+});
+
 app.get("/search", (req, res) => {
 	search({ location: req.query.q })
 	.then(results => {
